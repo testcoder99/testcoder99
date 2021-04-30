514f2eb727b4be7d3ba8064ccb0a1ba83b976c90
testcoder99 <81501880+testcoder99@users.noreply.github.com>
2021-04-30 10:10:19 +0500

    Warnas

--- Warnas/js/main.js
+++ Warnas/js/main.js
@@ -7,7 +7,18 @@ $(function(){
 	$('.characters__warning-button--close').on('click', function(event){
 		event.preventDefault();
 		$('.characters__warning').addClass('characters__warning--hidden')
-	})
+	});
+	$('.characters-content__tabs-item').click(function(event) {
+		event.preventDefault();
+		$('.characters-content__wrapper').not($(this)).removeClass('characters-content__wrapper--active');
+		$($(this).attr('href')).addClass('characters-content__wrapper--active');
+		if($('.characters-content__tabs-item').hasClass('characters-content__tabs-item--active')){
+		 		$('.characters-content__tabs-item').removeClass('characters-content__tabs-item--active');
+				$('.characters-content__tabs-item').not($(this)).removeClass('characters-content__tabs-item--active');
+		 };
+		$(this).addClass('characters-content__tabs-item--active');
+	});
+
 })	
 
 document.querySelector('.menu__btn').addEventListener('click', function(){
